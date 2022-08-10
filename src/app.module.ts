1d9d43182013351f9841fced79eb32f2b8eac761
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-08-10 06:57:00 +0200

    feat: use nestjs config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { AuthService } from './auth/auth.service';
 import { HolidayModule } from './holiday/holiday.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  controllers: [AuthController],
-  imports: [AuthModule, UserModule, HolidayModule, PrismaModule],
-  providers: [AuthService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    HolidayModule,
+    PrismaModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://savi-intranet:s@v1-1ntr@n3t@localhost:5432/savi-intranet?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
