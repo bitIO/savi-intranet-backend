2ad0150cd46c2a53c103f6871906d1ab194b25dc
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-08-10 16:21:14 +0200

    refactor(user): param decorator optional parameters

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,8 +1,13 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-const GetUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.user;
-});
+const GetUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    if (data) {
+      return request.user[data];
+    }
+    return request.user;
+  },
+);
 
 export { GetUser };
