d91d895a70f2b31c9af5089fa00f0666745c07d7
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-08-10 15:58:41 +0200

    fix(auth): strategy validation retrieves user

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
+  constructor(config: ConfigService, private prisma: PrismaService) {
     super({
       ignoreExpiration: false,
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -13,11 +14,31 @@ class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: any) {
-    return {
-      userId: payload.sub,
-      username: payload.username,
-    };
+  async validate(payload: {
+    email: string;
+    exp: number;
+    iat: number;
+    sub: number;
+  }) {
+    const user = this.prisma.user.findUnique({
+      select: {
+        createdAt: true,
+        email: true,
+        firstName: true,
+        id: true,
+        lastName: true,
+        updatedAt: true,
+      },
+      where: {
+        id: payload.sub,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
   }
 }
 
