6053cbeec2a80fa9c08fad77578d22b1e8a7c583
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-08-22 14:27:06 +0200

    fix(test): update user acceptance test

--- test/acceptance/user.e2e-spec.ts
+++ test/acceptance/user.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { INestApplication } from '@nestjs/common';
+import { Role } from '@prisma/client';
 import { addBusinessDays } from 'date-fns';
 import * as pactum from 'pactum';
 import { AuthDto } from '../../src/auth/dto';
@@ -13,11 +14,31 @@ import { createTestUsers, users } from '../utils/users';
 
 describe('APP Acceptance - User', () => {
   let app: INestApplication;
+  let dbUsers: { email: string; id: number }[];
+
+  function getIdForRole(role: Role) {
+    switch (role) {
+      case 'ADMIN':
+        return dbUsers.find((user) => {
+          return user.email.startsWith('admin');
+        }).id;
+      case 'APPROVE':
+        return dbUsers.find((user) => {
+          return user.email.startsWith('approve');
+        }).id;
+      case 'USER':
+        return dbUsers.find((user) => {
+          return user.email.startsWith('user');
+        }).id;
+      default:
+        throw new Error('invalid role');
+    }
+  }
 
   beforeAll(async () => {
     app = await initializeApplication('acceptance-user');
     await cleanDatabase(app);
-    await createTestUsers(app, 22);
+    dbUsers = await createTestUsers(app, 22);
 
     const credentials: AuthDto[] = users.map(({ email, password }) => {
       return {
@@ -50,129 +71,127 @@ describe('APP Acceptance - User', () => {
     app.close();
   });
 
-  describe('User', () => {
-    it('should get current user', () => {
+  it('should get current user', () => {
+    return pactum
+      .spec()
+      .get('/users/me')
+      .withHeaders({
+        Authorization: 'Bearer $S{userAt}',
+      })
+      .expectJsonLength('role', 1)
+      .expectJson('role', ['USER'])
+      .expectStatus(200);
+  });
+
+  describe('Create Holiday Request', () => {
+    const start = new Date();
+    const end = addBusinessDays(start, 7);
+
+    const createHolidayDto: CreateHolidayDto = {
+      end,
+      start,
+    };
+
+    it('should fail for exceed quota request', () => {
       return pactum
         .spec()
-        .get('/users/me')
+        .post('/holidays')
         .withHeaders({
           Authorization: 'Bearer $S{userAt}',
         })
-        .expectJsonLength('role', 1)
-        .expectJson('role', ['USER'])
-        .expectStatus(200);
+        .withBody({
+          end: addBusinessDays(new Date(), 40),
+          start: new Date(),
+        })
+        .expectStatus(409)
+        .expectBodyContains('Quota exceeded');
     });
 
-    describe('Create Holiday Request', () => {
-      const start = new Date();
-      const end = addBusinessDays(start, 7);
+    it('should create a holiday request', () => {
+      return pactum
+        .spec()
+        .post('/holidays')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .withBody(createHolidayDto)
+        .expectStatus(201)
+        .expectBodyContains(createHolidayDto.start)
+        .expectBodyContains(createHolidayDto.end);
+    });
 
-      const createHolidayDto: CreateHolidayDto = {
-        end,
-        start,
+    it('should fail for the same holiday request', () => {
+      return pactum
+        .spec()
+        .post('/holidays')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .withBody(createHolidayDto)
+        .expectStatus(409)
+        .expectBodyContains('Request already created');
+    });
+
+    it('should comment on request', () => {
+      const commentHolidayDto: CommentHolidayRequestDto = {
+        comment: 'user comment',
+        holidayRequestId: 1,
+        userId: 1,
+      };
+      pactum
+        .spec()
+        .post('/holidays/1/comments')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .withBody(commentHolidayDto)
+        .expectStatus(201)
+        .expectBody(commentHolidayDto.comment);
+    });
+  });
+
+  describe('Get holidays request', () => {
+    it('should retrieve my holiday requests', () => {
+      return pactum
+        .spec()
+        .get(`/holidays/users/${getIdForRole('USER')}`)
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .expectStatus(200)
+        .expectJsonLength(1);
+    });
+
+    it('should fail retrieving others holiday requests', () => {
+      return pactum
+        .spec()
+        .get('/holidays/users/20')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .expectStatus(403)
+        .expectBodyContains('Access to resource denied');
+    });
+  });
+
+  describe('Edit user', () => {
+    it('should edit user', () => {
+      const editUserDto: EditUserDto = {
+        email: 'test@savispain.es',
+        firstName: 'Name',
+        lastName: 'Surname User',
       };
 
-      it('should fail for exceed quota request', () => {
-        return pactum
-          .spec()
-          .post('/holidays')
-          .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
-          })
-          .withBody({
-            end: addBusinessDays(new Date(), 40),
-            start: new Date(),
-          })
-          .expectStatus(409)
-          .expectBodyContains('Quota exceeded');
-      });
-
-      it('should create a holiday request', () => {
-        return pactum
-          .spec()
-          .post('/holidays')
-          .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
-          })
-          .withBody(createHolidayDto)
-          .expectStatus(201)
-          .expectBodyContains(createHolidayDto.start)
-          .expectBodyContains(createHolidayDto.end);
-      });
-
-      it('should fail for the same holiday request', () => {
-        return pactum
-          .spec()
-          .post('/holidays')
-          .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
-          })
-          .withBody(createHolidayDto)
-          .expectStatus(409)
-          .expectBodyContains('Request already created');
-      });
-
-      it('should comment on request', () => {
-        const commentHolidayDto: CommentHolidayRequestDto = {
-          comment: 'user comment',
-          holidayRequestId: 1,
-          userId: 1,
-        };
-        pactum
-          .spec()
-          .post('/holidays/1/comments')
-          .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
-          })
-          .withBody(commentHolidayDto)
-          .expectStatus(201)
-          .expectBody(commentHolidayDto.comment);
-      });
-    });
-
-    describe('Get holidays request', () => {
-      it('should retrieve my holiday requests', () => {
-        return pactum
-          .spec()
-          .get('/holidays/users/1')
-          .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
-          })
-          .expectStatus(200)
-          .expectJsonLength(1);
-      });
-
-      it('should fail retrieving others holiday requests', () => {
-        return pactum
-          .spec()
-          .get('/holidays/users/2')
-          .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
-          })
-          .expectStatus(403)
-          .expectBodyContains('Access to resource denied');
-      });
-    });
-
-    describe('Edit user', () => {
-      it('should edit user', () => {
-        const editUserDto: EditUserDto = {
-          email: 'fcalle@savispain.es',
-          firstName: 'Francisco',
-          lastName: 'Calle Moreno',
-        };
-
-        return pactum
-          .spec()
-          .patch('/users')
-          .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
-          })
-          .withBody(editUserDto)
-          .expectStatus(200)
-          .expectBodyContains(editUserDto.firstName)
-          .expectBodyContains(editUserDto.email);
-      });
+      return pactum
+        .spec()
+        .patch('/users')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .withBody(editUserDto)
+        .expectStatus(200)
+        .expectBodyContains(editUserDto.firstName)
+        .expectBodyContains(editUserDto.email);
     });
   });
 });
