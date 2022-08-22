34abcb60a4567c677085a08a05023c78fe68731a
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-08-22 13:53:51 +0200

    feat(test): create test users

--- test/utils/users.ts
+++ test/utils/users.ts
@@ -1,3 +1,8 @@
+import { INestApplication } from '@nestjs/common';
+import { AuthService } from '../../src/auth/auth.service';
+import { PrismaService } from '../../src/prisma/prisma.service';
+import { UserService } from '../../src/user/user.service';
+
 const users = [
   {
     email: 'user@savispain.es',
@@ -13,4 +18,64 @@ const users = [
   },
 ];
 
-export { users };
+async function createTestUsers(
+  app: INestApplication,
+  remainingHolidays?: number,
+) {
+  const authService = await app.get(AuthService);
+  const userService = await app.get(UserService);
+
+  const dbUsers = await Promise.all(
+    users.map((user) => {
+      return authService.signUp({
+        email: user.email,
+        password: user.password,
+      });
+    }),
+  );
+
+  Promise.all(
+    dbUsers.map((user) => {
+      switch (user.email) {
+        case 'admin@savispain.es':
+          return userService.switchRole(user.id, {
+            firstName: 'admin',
+            role: ['USER', 'ADMIN'],
+          });
+        case 'approve@savispain.es':
+          return userService.switchRole(user.id, {
+            firstName: 'approve',
+            role: ['USER', 'APPROVE'],
+          });
+        case 'user@savispain.es':
+          return userService.switchRole(user.id, {
+            firstName: 'user',
+            role: ['USER'],
+          });
+        default:
+          throw new Error('Unexpected email');
+      }
+    }),
+  );
+
+  if (remainingHolidays) {
+    const prismaService = await app.get(PrismaService);
+    Promise.all(
+      dbUsers.map((user) => {
+        return prismaService.userHolidays.update({
+          data: {
+            remaining: remainingHolidays,
+          },
+          where: {
+            userId_year: {
+              userId: user.id,
+              year: new Date().getFullYear(),
+            },
+          },
+        });
+      }),
+    );
+  }
+}
+
+export { createTestUsers, users };
